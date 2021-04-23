b3a588eb651cdf98fb71efa436fc9ef0e88d90b3
Gaurav Kumar Yadav <gaurav712@protonmail.com>
2021-04-24 00:55:52 +0530

    better error handling and non-blocking IO

--- wpa_supplicant_control.cpp
+++ wpa_supplicant_control.cpp
@@ -53,6 +53,9 @@ WPASupplicantControl::WPASupplicantControl(std::string wlan_interface_name)
 
     /* issue the "ATTACH" command to wpa_supplicant to be able to receive events */
     this->send_cmd("ATTACH");
+    if ((this->get_response()) != "OK\n") {
+        qCritical() << "Error initializing event listener!";
+    }
 }
 
 /* Destructor */
@@ -68,11 +71,36 @@ void WPASupplicantControl::send_cmd(QString cmd) {
 
 /* Get responses from commands issued to wpa_supplicant */
 QString WPASupplicantControl::get_response() {
+
+    fd_set readfd;
+    int result;
+    struct timeval timeout;
     char buffer[MAX_BUFFER_LEN + 1];    // for the '\0'
 
-    recv(wpa_control_socket, buffer, MAX_BUFFER_LEN, 0);
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 1000;
 
-    return QString::fromStdString(buffer);
+    FD_ZERO(&readfd);
+    FD_SET(wpa_control_socket, &readfd);
+
+    if((result = select(wpa_control_socket + 1, &readfd, NULL, NULL, &timeout)) < 0) {
+        qCritical() << "Socket descriptor has no data to read!";
+        return QString::fromStdString("");
+    } else if(result == 0) {
+        qCritical() << "Timeout waiting for the socket descriptor!";
+        return QString::fromStdString("");
+    }
+
+    if(FD_ISSET(wpa_control_socket, &readfd)) {
+        if((recv(wpa_control_socket, buffer, MAX_BUFFER_LEN, 0)) < 0) {
+            qCritical() << "Failed to read from socket descriptor!";
+            return QString::fromStdString("");
+        } else {
+            return QString::fromStdString(buffer);
+        }
+    }
+
+    return QString::fromStdString("");
 }
 
 /* Spawn a new thread and scan for available networks */
